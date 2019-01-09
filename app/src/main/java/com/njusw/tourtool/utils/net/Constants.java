d3ff754f8f6041e8e8e72d79d5f0742c9f50114f
houyuguo <njtechhyg@163.com>
2019-01-09 14:10:41 +0800

    Some minor changes

---
+++ app/src/main/java/com/njusw/tourtool/utils/net/Constants.java
@@ -0,0 +1,12 @@
+package com.njusw.tourtool.utils.net;
+
+/**
+ * 一些常量的定义
+ *
+ * @author houyg
+ */
+public class Constants {
+    public static final String HOST_NAME = "172.19.240.146";
+    public static final int REQUEST_GET = 0;
+    public static final int REQUEST_POST = 1;
+}
